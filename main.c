81f24ede0332f9de6800f6a14f5be992dd794d18
Adrian Smith <adrianrsmith93@gmail.com>
2017-11-12 12:47:44 -0800

    minor changes

--- main.c
+++ main.c
@@ -4,18 +4,19 @@
 #include <string.h>
 #include <sys/wait.h>
 
+
 void arrcpy(char dest[][MAX_LINE_CHARS], char src[][MAX_LINE_CHARS], int length);
 
-int main() {
-    
-
-    return 0;
-}
-
-void arrcpy(char dest[][MAX_LINE_CHARS], char src[][MAX_LINE_CHARS], int length) {
-	for (int i = 0; i < length; i++) {
-		strcpy(dest[i], src[i]);
+int main(int argc, char *argv[]) {
+	char* filename;
+    if (argc != 2) {/* We print argv[0] assuming it is the program name */
+        printf( "usage: %s filename\n", argv[0] );
+		return 1;
+	} else {
+		filename = argc[1];
 	}
 	
-	return;
+	int** sudokuBoard = ParseFile(filename);
+
+    return 0;
 }
